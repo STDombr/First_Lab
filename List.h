cba8820247c41da9848b1ad2be2e52d42eb2ad73
Stas Dombrovskyi <727727stas@gmail.com>
2019-09-28 21:28:42 +0300

    Update List.h

--- List.h
+++ List.h
@@ -1,6 +1,7 @@
 #ifndef FIRST_LAB_LIST_H
 #define FIRST_LAB_LIST_H
 
+//Структура для зв'язних списків
 template <typename T>
 struct Node
 {
@@ -9,6 +10,7 @@ struct Node
     Node *prev;
 };
 
+//Клас, на основі зв'язних списків організовані алгоритми сортування
 template <typename T>
 class List
 {
@@ -16,20 +18,33 @@ private:
     Node<T> *head,*tail;
     int size;
 public:
+    //конструктор для класу List
     List();
+    //функція для видалення всієї інформації
     void remove();
+    //функція для добавляння одного листа
     void add(T);
+    //функція для виведення всього класу
     void print();
+    //функція для видалення листа tail
     void popTail();
+    //функція для видалення листа head
     void popHead();
+    //функція для повернення елемента з і індексом
     T element(int);
+    //функція для повернення листа з і інлексом
     Node<T> *elect_element(int);
+    //алгоритм сортування quicksort
     void quicksort(int=1,int=-2);
+    //алгоритм сортування insertionsort
     void insertionsort();
+    //алгоритм сортування mergesort
     void merge(int, int, int);
     void mergesort(int=1,int=-2);
+    //алгоритм сортування heapsort
     void heap(int,int);
     void heapsort();
+    //алгоритм сортування selectionsort
     void selectionsort();
 };
 
