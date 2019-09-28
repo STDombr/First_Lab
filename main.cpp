006c38be9482301ac4bd87b9fd865ae2cd8f8cb1
Stas Dombrovskyi <727727stas@gmail.com>
2019-09-28 21:28:35 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "Time.cpp"
+#include "List.cpp"
+
+using namespace std;
+
+int main()
+{
+    Time a;
+    int b[6];
+    for (int i=0;i<6;i++)
+    {
+     cin>>b[i];
+    }
+    //a.add(b[0],b[1],b[2],b[3],b[4],b[5]);
+    //a.print();
+}
