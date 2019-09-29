c71e91cc409335d3b5ac3f9e44a47f47ccd8b3f0
Stas Dombrovskyi <727727stas@gmail.com>
2019-09-30 00:31:14 +0300

    Update Time.cpp
    Створено функції для класу Time із перевіркою правильності дати

--- Time.cpp
+++ Time.cpp
@@ -2,9 +2,8 @@
 #include <windows.h>
 #include "Time.h"
 
-int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
-
-/*Time::Time()
+template <typename T>
+Time<T>::Time()
 {
     SYSTEMTIME st;
     GetSystemTime(&st);
@@ -14,18 +13,13 @@ int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
     hour=st.wHour;
     minute=st.wMinute;
     second=st.wSecond;
-}*/
+}
 
-/*bool leap(int y)
+template <typename T>
+void Time<T>::add(int day_1, int month_1, int year_1, int hour_1, int minute_1, int second_1)
 {
-    if (y%4==0 && y%100!=0 || y%400==0)
-        return 1;
-    else
-        return 0;
-}*/
 
-/*void Time::add(int year_1, int month_1, int day_1, int hour_1, int minute_1, int second_1)
-{
+    int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
 if (hour_1>=0 && hour_1<24 &&
         minute_1>=0 && minute_1<60 &&
         second_1>=0 && second_1<60 &&
@@ -37,19 +31,71 @@ if (hour_1>=0 && hour_1<24 &&
             number_of_days++;
         if (day_1>0 && day_1<=number_of_days)
         {
-            year=year_1;
-            month=month_1;
-            day=day_1;
-            hour=hour_1;
-            minute=minute_1;
-            second=second_1;
+            set_year(year_1);
+            set_month(month_1);
+            set_day(day_1);
+            set_hour(hour_1);
+            set_minute(minute_1);
+            set_second(second_1);
             return;
         }
     }
     std::cout<<"Invalid date!";
-}*/
+}
 
-void Time::print()
+template <typename T>
+void Time<T>::print()
 {
-    std::cout<<day<<"."<<month<<"."<<year<<" "<<hour<<":"<<minute<<":"<<second;
+    if (day/10==0)
+        std::cout<<"0";
+    std::cout<<day<<".";
+    if (month/10==0)
+        std::cout<<"0";
+    std::cout<<month<<".";
+    std::cout<<year<<" ";
+    if (hour/10==0)
+        std::cout<<"0";
+    std::cout<<hour<<":";
+    if (minute/10==0)
+        std::cout<<"0";
+    std::cout<<minute<<":";
+    if (second/10==0)
+        std::cout<<"0";
+    std::cout<<second;
+}
+
+template <typename T>
+void Time<T>::set_year(int a)
+{
+    this->year=a;
+}
+
+template <typename T>
+void Time<T>::set_month(int a)
+{
+    this->month=a;
+}
+
+template <typename T>
+void Time<T>::set_day(int a)
+{
+    this->day=a;
+}
+
+template <typename T>
+void Time<T>::set_hour(int a)
+{
+    this->hour=a;
+}
+
+template <typename T>
+void Time<T>::set_minute(int a)
+{
+    this->minute=a;
+}
+
+template <typename T>
+void Time<T>::set_second(int a)
+{
+    this->second=a;
 }
