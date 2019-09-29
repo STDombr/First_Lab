0c6c6a13e46362441955df05db88fae2607bd4e0
Stas Dombrovskyi <727727stas@gmail.com>
2019-09-30 00:31:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <vector>
-#include "Time.cpp"
-#include "List.cpp"
-
+#include "Test.cpp"
 using namespace std;
 
 int main()
 {
-    Time a;
-    int b[6];
-    for (int i=0;i<6;i++)
-    {
-     cin>>b[i];
-    }
-    //a.add(b[0],b[1],b[2],b[3],b[4],b[5]);
-    //a.print();
+    /*List<Time<int>> a;
+    a.gethead()->data;
+    Time<int> temp;
+    a.add(temp);
+    a.gethead()->data.print();
+*/
+    test_list_int<int>();
+    test_list_double<double>();
+    test_list_string<string>();
+
 }
+
