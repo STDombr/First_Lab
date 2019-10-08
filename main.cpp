55c3b7106200bf8ee0f8ecf650e868ade5eada78
Stas Dombrovskyi <727727stas@gmail.com>
2019-10-09 00:14:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,49 @@
 #include <iostream>
 #include "Test.cpp"
+//#include "Time.cpp"
+#include <ctime>
 using namespace std;
+
+void menu();
 
 int main()
 {
-    /*List<Time<int>> a;
-    a.gethead()->data;
-    Time<int> temp;
-    a.add(temp);
-    a.gethead()->data.print();
-*/
-    test_list_int<int>();
-    test_list_double<double>();
-    test_list_string<string>();
-
+    srand(time(0));
+    menu();
 }
 
+
+//функція для представлення в консоль основних варіантів роботи з програмою
+void menu()
+{
+    cout<<"1-Testing class Time"<<endl
+        <<"2-Testing class Date"<<endl
+        <<"3-Testing structure of List"<<endl
+        <<"4-Testing structure of Array"<<endl
+        <<"9-Exit"<<endl;
+    int i;
+    cin>>i;
+    if (i==9)
+        return;
+    if (i==3 || i==4)
+    {
+        std::cout<<std::endl<<std::endl;
+        test_list_int<int>(i);
+        test_list_double<double>(i);
+        test_list_string<string>(i);
+        test_list_class_Time<Time<int>>(i);
+        test_list_class_Date<Date<int>>(i);
+    }
+    if (i==1)
+    {
+        std::cout<<std::endl<<std::endl;
+        test_Time<int>();
+    }
+    if (i==2)
+    {
+        std::cout<<std::endl<<std::endl;
+        test_Date<int>();
+        menu();
+    }
+    menu();
+}
