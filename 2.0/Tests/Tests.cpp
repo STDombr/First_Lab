1bddeba1c6ef1e59cdc53dcb919fde9a26a0c304
Stas Dombrovskyi <727727stas@gmail.com>
2020-03-24 17:11:35 +0200

    Added first test

---
+++ 2.0/Tests/Tests.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "Interface.h"
+#include "Array.cpp"
+#include "List.cpp"
+#include "Vector.cpp"
+#include <gtest/gtest.h>
+
+
--- 2.0/main.cpp
+++ 2.0/main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <ctime>
-#include "Test.cpp"
-#include <gtest/gtest.h>
+//#include "Test.cpp"
+#include "Tests.cpp"
 
 using namespace std;
 
+class ListTest : public ::testing::Test{
+protected:
+    virtual void SetUp(){
+        l_2.add(1);
+        l_2.add(2);
+    }
+
+    List<int> l_1;
+    List<int> l_2;
+};
+
+TEST_F(ListTest, IsEmpty) {
+    EXPECT_EQ(0,l_1.GetSize());
+}
 
 
+/*TEST(BasicTests, TestName) {
+    List<int> L1;
+    L1.add(123);
+    EXPECT_EQ(123,L1.element(0));
+}*/
 
 int main()
 {
-    srand(time(0));
-    menu();
+    //srand(time(0));
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+    //menu();
 }
