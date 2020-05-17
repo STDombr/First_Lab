fbcceb78a1511722fdae8a715159bf6b593c6da6
Stas Dombrovskyi <727727stas@gmail.com>
2020-05-17 19:43:31 +0300

    Update main.cpp
    Add two type of running program:
    1 run Google tests
    2 run QT interface

--- 2.0/main.cpp
+++ 2.0/main.cpp
@@ -14,14 +14,23 @@ List<Date_and_Time> ListDate;
 
 int main(int argc, char* argv[])
 {
-   // srand(time(0));
-    //::testing::InitGoogleTest();
-    //return RUN_ALL_TESTS();
-    //menu();
-    //return 0;
+   srand(time(0));
+   int temp;
+   cout<<"1 - to run Google Tests"<<endl;
+    cout<<"2 - to run QT interface"<<endl;
+    cin>>temp;
+    if (temp == 1)
+    {
+        ::testing::InitGoogleTest();
+        return RUN_ALL_TESTS();
+    }
+    else if (temp == 2)
+    {
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
 
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    return 0;
 }
